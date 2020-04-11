f601495072074e8715356fdec220b25bdb556eda
Shaharna <shaharna1@gmail.com>
2020-04-11 17:05:15 +0300

    Tested, landscape and portrait works fine. More UI needed for the viewText.

--- app/src/main/java/com/example/postpc_shaharnahum/MainActivity.java
+++ app/src/main/java/com/example/postpc_shaharnahum/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.postpc_shaharnahum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.text);
+        final EditText editText = findViewById(R.id.inputText);
+        final Button createBtn = findViewById(R.id.createBtn);
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText());
+                editText.getText().clear();
+            }
+        });
     }
 }
